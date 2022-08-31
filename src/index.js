e2b7e6f12ba91500ad4f3eb97eb85c2fd0824b20
bkmohan <mohankrishnabk@gmail.com>
2022-08-31 23:35:59 +0530

    added icons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { format } from "date-fns";
 import { generateHourlyElements } from "./hourly_weather";
+import { cachedData, storeData } from "./init";
 import { getWeatherData } from "./weather";
+
 
 const dailyHeader = document.querySelector('.forecast-header__daily');
 const hourlyHeader = document.querySelector('.forecast-header__hourly');
@@ -11,11 +13,11 @@ const leftScrollArrow = document.querySelector('#arrow-left');
 const rightScrollArrow = document.querySelector('#arrow-right');
 const searchLocation = document.querySelector('#search-icon');
 const tempSwitch = document.querySelector('#unit-toogle');
-const dailtForecast = document.querySelectorAll('.forecast__daily_day');
 
 const location = document.querySelector('#location-and-date__location');
 const currentDate = document.querySelector('#location-and-date__current-date');
 const currentTime = document.querySelector('#location-and-date__current-time');
+const weatherIcon = document.querySelector('#weather__temperature__icon');
 const currentTemperature = document.querySelector('#weather__temperature__value');
 const currentSummary = document.querySelector('#weather__temperature__summary');
 
@@ -116,15 +118,65 @@ function capitalize(string){
     return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
 }
 
+function getIcon(weatherId){
+    switch(weatherId){
+        case '01d':
+            return './images/weather/sun.svg'
+        
+        case '01n':
+            return './images/weather/moon.svg'
+        
+        case '02d':
+            return './images/weather/cloudy-day.svg'
+        
+        case '02n':
+            return './images/weather/cloudy-night.svg'
+        
+        case '03d':
+        case '03n':
+            return './images/weather/cloud.svg'
+        
+        case '04d':
+        case '04n':
+            return './images/weather/cloudy.svg'
+       
+        case '9d':
+        case '9n':
+        case '10d':
+        case '10n':
+            return './images/weather/rainy.svg'
+        
+            case '011d':
+        case '0112':
+            return './images/weather/lightning.svg'
+        
+            case '13d':
+        case '13n':
+            return './images/weather/snow.svg'
+        
+            case '50d':
+        case '50n':
+            return './images/weather/mist.svg'
+    
+        default:
+            return './images/weather/cloudy.svg'
+                                                
+
+    }
+}
+
 function updateWeatherData(data){
     if(data){
+        document.querySelector('.location-error').style.display = 'none';
+
         location.textContent = data.location;
         let date = new Date(data.current.dt * 1000)
         currentDate.textContent = format(date, "EEEE, do MMM 'yy");
         currentTime.textContent = format(date, 'h:mm aaa');
         currentTemperature.innerHTML = getRelvantTemp(data.current.temp);
         currentSummary.textContent = capitalize(data.current.weather[0].description);
-        
+        weatherIcon.src = getIcon(data.current.weather[0].icon)
+
         feelsLike.innerHTML = getRelvantTemp(data.current.feels_like);
         precipitate.textContent = data.hourly[0].pop * 100 + ' %';
         humidity.textContent = data.current.humidity + ' %';
@@ -137,7 +189,7 @@ function updateWeatherData(data){
             dayForecast.querySelector('.day').textContent = format(day, 'EEEE')
             dayForecast.querySelector('.forecast__daily_high').innerHTML = getRelvantTemp(data.daily[i+1].temp.max);
             dayForecast.querySelector('.forecast__daily_low').innerHTML = getRelvantTemp(data.daily[i+1].temp.min);
-
+            dayForecast.querySelector('.forecast__icon').src = getIcon(data.daily[i+1].weather[0].icon)
             //TODO icon
         }
 
@@ -146,11 +198,25 @@ function updateWeatherData(data){
             let hourForecast = document.querySelector(`.forecast__hourly_day[data-id="${i}"]`)
             hourForecast.querySelector('.hour').textContent = format(hour, 'h:mm aaa')
             hourForecast.querySelector('.forecast__hourly_temperature').innerHTML = getRelvantTemp(data.hourly[i+1].temp);
-        
+            hourForecast.querySelector('.forecast__icon').src = getIcon(data.hourly[i+1].weather[0].icon)
             //TODO icon
         }
+        storeData(data.location, tempUnit)
     }
     else{
         document.querySelector('.location-error').style.display = 'block';
     }
 }
+
+async function init(){
+    let initData = cachedData();
+    if(initData.location){
+        WeatherData = await getWeatherData(initData.location);
+        if(initData.unit){
+            tempSwitch.checked = initData.unit == 'F' ? true : false;
+        }
+        updateWeatherData(WeatherData)
+    }
+}
+
+init()
